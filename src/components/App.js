f24eb8a1465e9495be7f2b8a383c47081c797b88
Yashjain1998 <126048815+Yashjain1998@users.noreply.github.com>
2023-04-23 23:40:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,31 @@
 import React, { useRef, useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  const startTime = useRef(0);
+  const startTime = useRef(Date.now());
   const intervalRef = useRef(0);
   const [currentTime, setCurrentTime] = useState(0);
   const [laps, setLaps] = useState([]);
-
-  return (
+  const handlestart=()=>{
+    clearInterval(intervalRef.current);
+    intervalRef.current=setInterval(() => {
+      setCurrentTime(Date.now()-startTime.current)
+    }, 10);
+  }
+ return (
     <div id="main">
       <section>
-        <h1 className='seconds-elapsed'>Stopwatch Time</h1>
-        <section className='buttons'>
-          <button className="start-btn">START</button>
-          <button className="stop-btn">STOP</button>
-          <button className="lap-btn">LAP</button>
-          <button className="reset-btn">RESET</button>
+        <h1 className='seconds-elapsed'>{(currentTime/1000).toFixed(3)}</h1>
+       <section className='buttons'>
+          <button className="start-btn" onClick={()=>handlestart()}>START</button>
+          <button className="stop-btn" onClick={()=>clearInterval(intervalRef.current)}>STOP</button>
+          <button className="lap-btn" onClick={()=>setLaps([...laps,currentTime/1000])}>LAP</button>
+          <button className="reset-btn" onClick={()=>{startTime.current=Date.now(),setCurrentTime(Date.now()-startTime.current)}}>RESET</button>
         </section>
       </section>
       <section className='lap-section'>
         <h2>Laps</h2>
         <section className='laps'>
-          <p>lap</p>
-          <p>lap</p>
-          <p>lap</p>
+          {laps.map(lap=><p>{lap}</p>)}
         </section>
       </section>
     </div>
